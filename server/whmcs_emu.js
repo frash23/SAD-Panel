3795eb08c5d86384bb2fef9272fddc44028d52b8
frash23 <frash23@cotse.net>
2014-07-27 13:51:19 +0200

    Add sqlite3 nodejs module

---
+++ server/whmcs_emu.js
@@ -0,0 +1,25 @@
+var net = require('net');
+
+var HOST = '127.0.0.1';
+var PORT = 21300;
+
+var client = new net.Socket();
+client.connect(PORT, HOST, function() {
+
+    client.write('SRV__START;PASSWUUUUT:KEK;SERVID:LELOKEK123');
+
+});
+
+// Add a 'data' event handler for the client socket
+// data is what the server sent to this socket
+client.on('data', function(data) {
+    
+    console.log('Data was recieved from master: '+data);
+    // Close the client socket completely
+    
+});
+
+// Add a 'close' event handler for the client socket
+client.on('close', function() {
+    console.log('Connection closed');
+});
